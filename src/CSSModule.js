33be7dca34a10f88e7ffb27456ee70f9d4518692
김지호 <jiho001108@gmail.com>
2024-04-18 09:11:06 +0900

    CSSModule의 클래스 두 개 이상 적용하기

--- src/CSSModule.js
+++ src/CSSModule.js
@@ -4,7 +4,7 @@ const CSSModule = () => {
 	console.log(styles);
 
 	return (
-		<div className={styles.wrapper}>
+		<div className={`${styles.wrapper} ${styles.inverted}`}>
 			안녕하세요, 저는 <span className="something">CSS Module!</span>
 		</div>
 	);
