8b8b4a6c6b3d0dec8c065440dc940951056445ea
김지호 <jiho001108@gmail.com>
2024-04-18 11:10:01 +0900

    CSS Module + classnames

--- src/CSSModule.js
+++ src/CSSModule.js
@@ -1,10 +1,13 @@
+import classNames from "classnames";
 import styles from "./CSSModule.module.css";
 
+const cx = classNames.bind(styles); // 미리 styles에서 클래스를 받아 오도록 설정
+
 const CSSModule = () => {
-	console.log(styles);
 
 	return (
-		<div className={`${styles.wrapper} ${styles.inverted}`}>
+		// <div className={`${styles.wrapper} ${styles.inverted}`}>
+      <div className={cx("wrapper", "inverted")}>
 			안녕하세요, 저는 <span className="something">CSS Module!</span>
 		</div>
 	);
